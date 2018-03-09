3be90db32f0713ada85ec82b33bab02a7d024f88
hillliam <liamchat@gmail.com>
2018-03-09 23:54:29 +0000

    add local space calculation
    and code based object initalization

--- Object3D.cpp
+++ Object3D.cpp
@@ -7,11 +7,91 @@ Object3D::Object3D()
   vertexdata=NULL;
   polygons=NULL;
   name=NULL;
-    
+  SetName("circle");
   translation[0]=translation[1]=translation[2]=0.0f;
+  local[0] = local[1] = local[2] = 0.0f;
+  speclevel=5;
+  glossiness=25;
     
   elementcount=0;
   elementtype=GL_TRIANGLES;    
+
+  ambient[0]=ambient[1]=ambient[2]=0.2f;
+  ambient[3]=1.0f;
+  
+  diffuse[0]=diffuse[1]=diffuse[2]=0.8f;
+  diffuse[3]=1.0f;
+
+  specular[0]=specular[1]=specular[2]=0;
+  specular[3]=1.0f;
+    
+  texturemap=-1;
+}
+
+void Object3D::circle()
+{
+	const int n = 3*300;
+	const int NOOFVERTS = (n + 1);
+	const int NOOFTRIANGLES = n;
+	const int size2 = 3 * NOOFVERTS * sizeof(float);
+	vertexdata = (float*)malloc(size2);
+	this->noofverts = NOOFVERTS;
+	const int size = 3 * NOOFTRIANGLES * sizeof(unsigned short);
+	polygons = (unsigned short*)malloc(size);
+	elementcount = 3 * NOOFTRIANGLES;
+	
+	vertexdata[0] = 0;
+	vertexdata[1] = 0;
+	vertexdata[2] = -1;
+
+	for (int i = 0; i < n; i += 3)
+	{
+		float rads = DEGSTORADS(i*(360.0 / n));
+		float x = sin(rads * 0.9);
+		float y = cos(rads * 0.9);
+		vertexdata[i] = x;
+		vertexdata[i+1] = y;
+		vertexdata[i+2] = -1;
+	}
+	for (int i = 2; i < n; i++)
+	{
+			polygons[i - 2] = (i - 2) *3;
+			polygons[i - 1] = (i - 1) * 3;
+			polygons[i] = i * 3;
+	}
+	polygons[n - 1] = 0;
+	polygons[n] = 1;
+	polygons[n + 1] = n;
+}
+
+Object3D::Object3D(bool circledraw)
+{
+	vbos = NULL;
+	incuv = false;
+	vertexdata = NULL;
+	polygons = NULL;
+	name = NULL;
+
+	translation[0] = translation[1] = translation[2] = 0.0f;
+	local[0] = local[1] = local[2] = 0.0f;
+	speclevel = 5;
+	glossiness = 25;
+
+	elementcount = 0;
+	elementtype = GL_TRIANGLES;
+
+	ambient[0] = ambient[1] = ambient[2] = 0.2f;
+	ambient[3] = 1.0f;
+
+	diffuse[0] = diffuse[1] = diffuse[2] = 0.8f;
+	diffuse[3] = 1.0f;
+
+	specular[0] = specular[1] = specular[2] = 0;
+	specular[3] = 1.0f;
+
+	texturemap = -1;
+
+	circle();
 }
 
 Object3D::~Object3D()
@@ -20,6 +100,13 @@ Object3D::~Object3D()
   free(vertexdata);
   free(polygons);
   free(name);
+}
+
+void Object3D::getlocalmove(Object3D* root)
+{
+	local[0] = root->translation[0] - this->translation[0];
+	local[1] = root->translation[1] - this->translation[1];
+	local[2] = root->translation[2] - this->translation[2];
 }
 
 void Object3D::InitVBOs()
@@ -69,28 +156,77 @@ void Object3D::SetTranslation(float x, float y, float z)
   translation[2]=z;
 }
   
-  
-void Object3D::Draw(RenderingContext rcontext)
+void Object3D::SetMaterial(byte* buffer)
 {
+  int i;
+  int offset=0;
+  for (i=0; i<4; i++, offset+=4)
+  {
+    ambient[i]=*(float*) (buffer+offset);
+    diffuse[i]=*(float*) (buffer+offset+16);
+    specular[i]=*(float*) (buffer+offset+32);
+  }
+
+  WORD tmp=*(WORD*) (buffer+48);
+  glossiness=(float) tmp;
+  if (glossiness>100)
+    glossiness=100;
+
+  tmp=*(WORD*) (buffer+50);
+  speclevel=(float) tmp;
+
+  if (speclevel>100)
+    speclevel=100;
+
+  float spec=speclevel/100.0f;
+  for (int i=0; i<4; i++)
+    specular[i]*=spec;
+}
+  
+void Object3D::Draw(RenderingContext* rcontext)
+{
+  // If this method gives you any errors, make sure you update with the correct shader program handles for the uniforms and attributes
+  // You may also need to make sure your matrix class is working correctly
+
+  // Material
+  glUniform4fv(rcontext->mathandles[0], 1, ambient);
+  glUniform4fv(rcontext->mathandles[1], 1, diffuse);
+  glUniform4fv(rcontext->mathandles[2], 1, specular);
+  glUniform1f(rcontext->mathandles[3], glossiness);
+
   if (!vbos)
     InitVBOs();
     
-  rcontext.PushModelMatrix();
-    rcontext.Translate(translation[0], translation[1], translation[2]);
-    rcontext.UpdateMVPs();
-    glUniformMatrix4fv(rcontext.mvhandle, 1, false, rcontext.mvmatrix);
-    glUniformMatrix4fv(rcontext.mvphandle, 1, false, rcontext.mvpmatrix);
+  //rcontext->PushModelMatrix();
+    //rcontext->Translate(translation[0], translation[1], translation[2]);
+  rcontext->Translate(local[0], local[1], local[2]);
+  rcontext->PushModelMatrix();
+    rcontext->UpdateMVPs();
+    glUniformMatrix4fv(rcontext->mvhandle, 1, false, rcontext->mvmatrix);
+    glUniformMatrix4fv(rcontext->mvphandle, 1, false, rcontext->mvpmatrix);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbos[0]);    
-
     // Attributes
-    glVertexAttribPointer(rcontext.verthandles[0], 3, GL_FLOAT, false, 4*6, (void*) 0);
-    glVertexAttribPointer(rcontext.verthandles[1], 3, GL_FLOAT, false, 4*6, (void*) (4*3));
-    glEnableVertexAttribArray(rcontext.verthandles[0]);
-    glEnableVertexAttribArray(rcontext.verthandles[1]);
+    if (incuv)
+    {
+      glVertexAttribPointer(rcontext->verthandles[0], 3, GL_FLOAT, false, 4*8, (void*) 0);
+      glVertexAttribPointer(rcontext->verthandles[1], 3, GL_FLOAT, false, 4*8, (void*) (4*3));
+     //glVertexAttribPointer(rcontext->verthandles[2], 2, GL_FLOAT, false, 4*8, (void*) (4*6));
+      glEnableVertexAttribArray(rcontext->verthandles[0]);
+      glEnableVertexAttribArray(rcontext->verthandles[1]);
+     //glEnableVertexAttribArray(rcontext->verthandles[2]);
+    }
+    else
+    {
+      glVertexAttribPointer(rcontext->verthandles[0], 3, GL_FLOAT, false, 4*6, (void*) 0);
+      glVertexAttribPointer(rcontext->verthandles[1], 3, GL_FLOAT, false, 4*6, (void*) (4*3));
+      glEnableVertexAttribArray(rcontext->verthandles[0]);
+      glEnableVertexAttribArray(rcontext->verthandles[1]);
+      //glDisableVertexAttribArray(rcontext->verthandles[2]);
+    }
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbos[1]);    
     glDrawElements(elementtype, elementcount, GL_UNSIGNED_SHORT, 0);
 
-  rcontext.PopModelMatrix();
+  rcontext->PopModelMatrix();
 }
--- Object3D.h
+++ Object3D.h
@@ -6,15 +6,22 @@ class Object3D
 {
 public:
   Object3D();
+  Object3D(bool circledraw);
   ~Object3D();
+  float local[3];
 
   void SetName(const char* name);
-
+  char* getName();
   void SetVertexData(byte* buffer, int noofverts, int bufferlen);
   void SetTriangles(byte* buffer, int noofpolys);
+  void getlocalmove(Object3D* root);
+  void Draw(RenderingContext* rcontext);
 
-  void Draw(RenderingContext rcontext);
-
+  void SetMaterial(byte* buffer);
+  void SetDiffuse(float r, float g, float b, float a);
+  const float* GetDiffuse();
+  void SetTextureMap(int id);
+  
   void SetTranslation(byte* buffer);
   void SetTranslation(float x, float y, float z);
 
@@ -30,12 +37,39 @@ private:
   float* vertexdata;
   unsigned short* polygons;
   
+  float glossiness, speclevel;
+  float ambient[4], diffuse[4], specular[4];
+
   unsigned int* vbos;
   
+  int texturemap;
 
   void InitVBOs();
-
+  void circle();
 };
+
+inline void Object3D::SetTextureMap(int id)
+{
+  texturemap=id;
+}
+
+inline void Object3D::SetDiffuse(float r, float g, float b, float a)
+{
+  diffuse[0]=r;
+  diffuse[1]=g;
+  diffuse[2]=b;
+  diffuse[3]=a;
+}
+  
+inline char* Object3D::getName()
+{
+	return name;
+}
+
+inline const float* Object3D::GetDiffuse()
+{
+  return diffuse;
+}
 
 inline void Object3D::SetName(const char* name)
 {
