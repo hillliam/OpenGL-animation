2cd999b7d0fc5e0cf969f43879f1823e265c5259
hillliam <liamchat@gmail.com>
2018-05-01 23:36:47 +0100

    move round

--- GLSetup.h
+++ GLSetup.h
@@ -5,11 +5,11 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
-#include "GL/glut.h"
 #include "GL/glew.h"
 //#include "GL\eglew.h"
 //#include "GL\glxew.h"
 #include "GL/wglew.h"
+#include "GL/glut.h"
 
 HDC InitGL(HWND parent);
 int LoadShaders(const wchar_t* vertshader, const wchar_t* fragshader);
